2978d9b7f88af1a46cc0fb85dd6a68498b2be47d
nikvi <nikki.vinayan@gmail.com>
2024-06-11 17:33:41 -0400

    added function to notebook

--- Week 2/Day 1/aimakerspace/vectordatabase.py
+++ Week 2/Day 1/aimakerspace/vectordatabase.py
@@ -12,6 +12,13 @@ def cosine_similarity(vector_a: np.array, vector_b: np.array) -> float:
     norm_b = np.linalg.norm(vector_b)
     return dot_product / (norm_a * norm_b)
 
+def minkowski_distance(vector_a: np.array, vector_b: np.array, p_value=2) -> float:
+    return np.sum(np.abs(vector_a - vector_a)**p_value, axis=1)**(1/p_value)
+
+def euclidean_distance(vector_a: np.array, vector_b: np.array) -> float:
+    norm_a = np.linalg.norm(vector_a)
+    norm_b = np.linalg.norm(vector_b)
+    return np.linalg.norm(norm_a - norm_b)
 
 class VectorDatabase:
     def __init__(self, embedding_model: EmbeddingModel = None):
@@ -25,7 +32,7 @@ class VectorDatabase:
         self,
         query_vector: np.array,
         k: int,
-        distance_measure: Callable = cosine_similarity,
+        distance_measure: Callable = euclidean_distance,
     ) -> List[Tuple[str, float]]:
         scores = [
             (key, distance_measure(query_vector, vector))
@@ -37,7 +44,7 @@ class VectorDatabase:
         self,
         query_text: str,
         k: int,
-        distance_measure: Callable = cosine_similarity,
+        distance_measure: Callable = euclidean_distance,
         return_as_text: bool = False,
     ) -> List[Tuple[str, float]]:
         query_vector = self.embedding_model.get_embedding(query_text)
